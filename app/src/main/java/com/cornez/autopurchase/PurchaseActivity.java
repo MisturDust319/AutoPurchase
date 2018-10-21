62897e653fe3007974e0fbc2181a26be5247df5c
Stan Slupecki <sslup@hermetikos.com>
2018-10-21 18:12:29 -0400

    Adds Car Cost fragment

--- app/src/main/java/com/cornez/autopurchase/PurchaseActivity.java
+++ app/src/main/java/com/cornez/autopurchase/PurchaseActivity.java
@@ -115,7 +115,7 @@ public class PurchaseActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_car_cost) {
             return true;
         }
         return super.onOptionsItemSelected(item);
